1ce16201cb64d1a805c55df7d102ec7c089e603a
ksenia-gezalova <ksenia.gezalova@gmail.com>
2018-11-28 14:40:03 +0300

    finish with search and dates

--- main.js
+++ main.js
@@ -1,45 +1,67 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        showMode: 'departure',
-        schedule: [],
-        delayList: [],
-        direction: null,
-        search: ''
-    },
-    methods: {
-        loadSchedule: function () {
-            var self = this;
-            if (self.showMode === 'departure') {
-                self.direction = 'departure';
-            } else if (self.showMode === 'arrival') {
-                self.direction = 'arrival'
-            } else {
-                self.direction = 'all'
-            }
-            axios.get('http://localhost:11111/bitrix/timetable/', {
-                    params: {
-                        direction: self.direction,
-                        dateStart: '2018-11-27T18:00:00+03:00',
-                        dateEnd: '2018-11-27T20:00:00+03:00',
-                        perPage: 9999,
-                        page: 0,
-                        locale: 'ru'
-                    }
-                })
-                .then(function (response) {
-                    self.schedule = response.data.items;
-                    self.delayList = self.schedule.filter(function (s) {
-                        return s.t_otpr > s.t_st;
-                    });
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
-    },
-    created: function () {
-        var self = this;
-        this.loadSchedule();
-    }
-})
+	el: "#app",
+	data: {
+		showMode: "departure",
+		schedule: [],
+		delayList: [],
+		direction: null,
+		search: "",
+		startDate: new Date(),
+		endDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+	},
+	methods: {
+		loadSchedule: function() {
+			var self = this;
+			if (self.showMode === "departure") {
+				self.direction = "departure";
+			} else if (self.showMode === "arrival") {
+				self.direction = "arrival";
+			} else {
+				self.direction = "all";
+			}
+			axios
+				.get("http://localhost:11111/bitrix/timetable/", {
+					params: {
+						direction: self.direction,
+						dateStart: self.startDate,
+						dateEnd: self.endDate,
+						perPage: 9999,
+						page: 0,
+						locale: "ru"
+					}
+				})
+				.then(function(response) {
+					self.schedule = [];
+					self.schedule = response.data.items;
+					self.delayList = self.schedule.filter(function(s) {
+						return s.t_otpr > s.t_st;
+					});
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		},
+		editDate: function(date) {
+			date.toLocaleString("ru", self.options);
+		}
+	},
+	created: function() {
+		var self = this;
+		self.loadSchedule();
+	},
+	computed: {
+		filteredList() {
+			var self = this;
+			var filterObj =
+				this.showMode === "delay" ? this.delayList : this.schedule;
+			if (this.search === "") {
+				return filterObj;
+			}
+			return filterObj.filter(function(item) {
+				return item.flt
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+			});
+		}
+	}
+});
